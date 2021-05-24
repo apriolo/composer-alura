4287e3e89e0126b26a4f17c0df6804a262478b67
Arian Priolo <arianpriolo@gmail.com>
2021-05-24 15:08:56 -0300

    First Commit

---
+++ Teste.php
@@ -0,0 +1,17 @@
+
+<?php
+
+class Teste {
+    public static function metodo()
+    {
+        echo "test";
+    }
+}
+
+class Test2
+{
+    public static function FunctionName(Type $var = null)
+    {
+        echo "teste2";
+    }
+}
